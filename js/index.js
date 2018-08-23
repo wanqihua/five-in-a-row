6b0276f724d86ea774605cb6380f1e5d9192dd5c
wanqh <wqhnice@163.com>
2018-08-23 09:22:34 +0800

    8.23 在再来一局优化

--- js/index.js
+++ js/index.js
@@ -3,20 +3,13 @@ let globals = {
   firstPlay: true,                                  // firstPlay = true 定义黑棋先下
   playList: [],                                     // 棋谱中被占用的位置统计数组
   canvas: document.getElementById('canvas'),        // 画布元素
-  context: canvas.getContext('2d'),                 // 获取2d上下文对象
+  context: this.canvas.getContext('2d'),                 // 获取2d上下文对象
   winList: [],                                      // 赢法数组
   count: 0,                                         // 赢法总数 1020
   blackWinList: [],                                 // 黑棋 赢法统计
   whiteWinList: [],                                 // 白棋 赢法统计
   gameOver: false,                                  // 是否已结束
 };
-
-for( let i = 0; i < 19; i++ ){
-  globals.playList[i] = [];
-  for( let j = 0; j < 19; j++ ){
-    globals.playList[i][j] = 0;
-  }
-}
 
 /***********赢法数组填充开始**************/
 const beginTime = new Date();
@@ -65,13 +58,13 @@ for( let i = 0; i < 15; i++ ){
 const endTime = new Date();
 console.log(endTime-beginTime);
 
-for( let i = 0; i < globals.count; i++ ){
-  globals.blackWinList[i] = 0;
-  globals.whiteWinList[i] = 0;
-}
+
+
 /***********赢法数组填充完毕**************/
 
 window.addEventListener('load',function(){
+  initPlayList();
+  initBWWinList();
   drawWatermark();
   drawLine();
 },false);
@@ -80,9 +73,7 @@ window.addEventListener('load',function(){
  * 落子
  */
 globals.canvas.addEventListener('click',function(e){
-  if( globals.gameOver ){
-    return
-  }
+  if( globals.gameOver ){return}
   let X = e.offsetX;
   let Y = e.offsetY;
   let x = Math.floor(X / 30);
@@ -94,10 +85,28 @@ globals.canvas.addEventListener('click',function(e){
     }else{
       globals.playList[x][y] = 2;
     }
-    judgement(x,y,globals.playList[x][y]);  //调用judgement函数
+    judgement(x,y,globals.playList[x][y]);   //调用judgement函数
     globals.firstPlay = !globals.firstPlay;
   }
 },false);
+
+//初始化被占用的位置统计数组
+function initPlayList(){
+  for( let i = 0; i < 19; i++ ){
+    globals.playList[i] = [];
+    for( let j = 0; j < 19; j++ ){
+      globals.playList[i][j] = 0;
+    }
+  }
+}
+
+// 初始化赢棋统计
+function initBWWinList(){
+  for( let i = 0; i < globals.count; i++ ){
+    globals.blackWinList[i] = 0;
+    globals.whiteWinList[i] = 0;
+  }
+}
 
 /**
  * 画水印、填充定义
@@ -106,8 +115,10 @@ function drawWatermark(){
   globals.context.translate(0.5,0.5);
   globals.context.fillStyle = 'rgba(255,255,255,.2)';
   globals.context.strokeStyle = '#333';
-  globals.context.font = "150px sans-serif";
-  globals.context.fillText("五子棋", 60, 340);
+  globals.context.font = "100 150px sans-serif";
+  globals.context.textAlign = 'center';
+  globals.context.textBaseline = 'middle';
+  globals.context.fillText("五子棋", 285, 285);
 }
 
 /**
@@ -115,12 +126,14 @@ function drawWatermark(){
  */
 function drawLine(){
   for( let i = 0; i < 19; i++ ){
+    globals.context.beginPath();
     globals.context.moveTo(15 + i*30, 15);
     globals.context.lineTo(15 + i*30, 555);
     globals.context.stroke();
     globals.context.moveTo(15, 15 + i*30);
     globals.context.lineTo(555, 15 + i*30);
     globals.context.stroke();
+    globals.context.closePath();
   }
 }
 
@@ -148,21 +161,46 @@ function play(x,y,person){
  */
 function judgement(x,y,color){
   for(let i = 0; i < globals.count; i++){
-    if( globals.winList[x][y][i] ){  //点(x,y)处有赢法
+    if( globals.winList[x][y][i] ){       //点(x,y)处有赢法
       if(color === 1){
         globals.blackWinList[i]++;
         if(globals.blackWinList[i] === 5){
-          console.log("黑子胜利!");
+          alertWin("黑子胜利!");
           globals.gameOver = true;
         }
       }
       if(color === 2){
         globals.whiteWinList[i]++;
         if(globals.whiteWinList[i] === 5){
-          console.log("白子胜利!");
+          alertWin("白子胜利!");
           globals.gameOver = true;
         }
       }
     }
   }
 }
+
+// setInterval(function(){alertWin('..')},500); 测试偏移
+// 胜利弹框
+function alertWin(str){
+  let alertBox = document.getElementById('alert_box');
+  let titleBox = document.getElementsByClassName('title')[0];
+  let buttonBox = document.getElementsByClassName('button_box')[0];
+  alertBox.style.display = 'block';
+  titleBox.innerHTML = str;
+  // buttonBox.click(); 模拟点击事件
+  buttonBox.addEventListener('click',function(){
+    globals.context.clearRect(0,0,570,570);
+    globals.canvas.width = globals.canvas.width;               // 写上这句话后，canvas重绘后canvas不会发生偏移
+    globals.canvas = document.getElementById('canvas');        // 画布元素
+    globals.context = globals.canvas.getContext('2d');         // 获取2d上下文对象
+    initPlayList();
+    initBWWinList();
+    drawWatermark();
+    drawLine();
+    globals.gameOver = false;
+    globals.firstPlay = true;
+    alertBox.style.display = 'none';
+  },false);
+}
+
